5f136209fbc1086dca426bf4fc0613ac547314cb
ibn05 <arikibnur@gmail.com>
2022-06-05 22:03:50 +0700

    Trying

--- index.js
+++ index.js
@@ -6,25 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, callback) =>
+    callback(names).map((item, i) => `${i + 1}. ${item}`);
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => names.sort();
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => names.sort().reverse();
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
+    console.log(sorter?.(names, sortAscending)?.join("\n"));
+    console.log(sorter?.(names, sortDescending)?.join("\n"));
 })();
 
 module.exports = {
-  sorter,
-  sortAscending,
-  sortDescending,
-  names,
+    sorter,
+    sortAscending,
+    sortDescending,
+    names,
 };
